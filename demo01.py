5ba2a30d0bf602b843c5038f68126743b617a189
Willie <1036539667@qq.com>
2020-05-28 21:48:29 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,12 @@
+"""print("hello world!")  # 字符串
+print(123)   #  整数
+print(2.11)  #  小数
+print(True)  #  布尔值
+print(())    #  元组
+print([])    #  数组
+print({})    #  字典 """
+
+
+a=input("请输入")
+b=input("请输入")
+print(len(a+b))
